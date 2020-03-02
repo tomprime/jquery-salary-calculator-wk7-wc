e2b5d10540bba89ec717193a9af29ecd53362791
tomprime <tom@primeacademy.io>
2020-03-02 13:03:55 -0600

    first commit of index.html, client.js, styles.css

---
+++ scripts/client.js
@@ -0,0 +1,15 @@
+let garage = [];
+
+/*
+Do not change newCar for base mode!
+*/
+function newCar(yearInput, makeInput, modelInput){
+  console.log('in newCar:', yearInput, makeInput, modelInput);
+  const newCarObject = {
+    year: yearInput,
+    make: makeInput,
+    model: modelInput
+  }
+  garage.push(newCarObject);
+  return true;
+} // end newCar
