cc99c9aac75449a90e0da44fc88bcb1077fe4993
tomprime <tom@primeacademy.io>
2020-03-02 15:54:02 -0600

    Added Add Employee form.

--- scripts/client.js
+++ scripts/client.js
@@ -1,15 +1,36 @@
-let garage = [];
+// what_01.js
 
-/*
-Do not change newCar for base mode!
-*/
-function newCar(yearInput, makeInput, modelInput){
-  console.log('in newCar:', yearInput, makeInput, modelInput);
-  const newCarObject = {
-    year: yearInput,
-    make: makeInput,
-    model: modelInput
-  }
-  garage.push(newCarObject);
-  return true;
-} // end newCar
+// Step 1
+let firstNumber = 4;
+let secondNumber = '2';
+
+let thirdNumber = theDivider(firstNumber, secondNumber);
+
+console.log('thirdNumber after step 1:', thirdNumber);
+
+// Step 2
+let fourthNumber = '1';
+
+if (thirdNumber) {
+    fourthNumber += thirdNumber;
+}
+
+console.log('fourthNumber after step 2:', fourthNumber);
+
+// Step 3
+let fifthNumber = theAddinator(firstNumber, fourthNumber, secondNumber);
+
+console.log('fifthNumber after step 3:', fifthNumber);
+
+// Step 4
+let sixthNumber = theAddinator(fifthNumber, fourthNumber, thirdNumber);
+
+console.log('sixthNumber after step 4:', sixthNumber);
+
+function theDivider(numberOne, numberTwo) {
+    return numberOne / numberTwo;
+}
+
+function theAddinator(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
