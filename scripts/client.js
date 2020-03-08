f9612895594dab0e970dfdc7e3d0ca67bb78b3a4
tomprime <tom@primeacademy.io>
2020-03-07 22:46:30 -0600

    Added styling and JS fuctions to add employees and remove employee rows.

--- scripts/client.js
+++ scripts/client.js
@@ -1,36 +1,79 @@
-// what_01.js
+let employees = [];
 
-// Step 1
-let firstNumber = 4;
-let secondNumber = '2';
+$(document).ready(onReady);
 
-let thirdNumber = theDivider(firstNumber, secondNumber);
-
-console.log('thirdNumber after step 1:', thirdNumber);
-
-// Step 2
-let fourthNumber = '1';
-
-if (thirdNumber) {
-    fourthNumber += thirdNumber;
+//run after DOM loads: 
+function onReady() {
+    console.log("In onReady() function.");
+    $('#submitButton').on('click', handleSubmitClick);
+    $('#employeeTableBody').on('click', 'button', removeRow );
 }
 
-console.log('fourthNumber after step 2:', fourthNumber);
+function handleSubmitClick(event) {
+    console.log("In handleSubmitClick() function.");
+    event.preventDefault();
+    const firstNameInput = $('#firstNameInput').val();
+    const lastNameInput = $('#lastNameInput').val();
+    //TODO Check that ID is unique, not used already, 
+    //before inserting a new employee: 
 
-// Step 3
-let fifthNumber = theAddinator(firstNumber, fourthNumber, secondNumber);
-
-console.log('fifthNumber after step 3:', fifthNumber);
-
-// Step 4
-let sixthNumber = theAddinator(fifthNumber, fourthNumber, thirdNumber);
-
-console.log('sixthNumber after step 4:', sixthNumber);
-
-function theDivider(numberOne, numberTwo) {
-    return numberOne / numberTwo;
+    const iDInput = $('#iDInput').val();
+    const titleInput = $('#titleInput').val();
+    const annualSalaryInput = $('#annualSalaryInput').val();
+    employees.push({firstNameInput, lastNameInput, iDInput, titleInput, annualSalaryInput});
+    console.log("employees[] is:", employees);
+    displayEmployees();
 }
 
-function theAddinator(numberOne, numberTwo) {
-    return numberOne + numberTwo;
+function removeRow(event) {
+    console.log("In removeRow() function.");
+    console.log("this.parent() is:", $(this).parent().parent());
+    $(this).parent().parent().remove();
+    //${this} example:
+    //<button class="button" id="3456">Delete</button>
+    //remove employee from employees array:
+    const employeeId = $(this).attr('id');
+    for(let i = 0; i < employees.length; i++) {
+        if(employees[i].iDInput === employeeId) {
+            employees.splice(i, 1);
+        }
+    }
+    console.log("employees[] is:", employees);
+    //"redraw" with displayEmployees() after 
+    //employees[] was updated: 
+    displayEmployees();
+}
+
+/* displayEmployees() function loops 
+ * over each employee object in the employees 
+ * array, and appends each employee object to
+ * the DOM. 
+ */
+function displayEmployees() {
+    console.log("In displayEmployees() function.");
+    $('#employeeTableBody').empty();
+    for(let employee of employees) {
+        console.log('employee is:', employee);
+        const row = `<tr><td>${employee.firstNameInput}</td>
+                    <td>${employee.lastNameInput}</td> 
+                    <td>${employee.iDInput}</td> 
+                    <td>${employee.titleInput}</td> 
+                    <td>${employee.annualSalaryInput}</td> 
+                    <td><button class="button" id="${employee.iDInput}">Delete</button></td></tr>`;
+        $('#employeeTableBody').append(row);
+    }
+    displaySalaryTotal();
+}
+
+function displaySalaryTotal() {
+    let salaryTotal = 0;
+    for(let employee of employees) {
+        salaryTotal += Number(employee.annualSalaryInput);
+    }
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    $('#salariesTotal').empty();
+    $('#salariesTotal').append(formatter.format(salaryTotal));
 }
